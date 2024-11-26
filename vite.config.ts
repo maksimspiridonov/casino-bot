b8aa740083e32ad8ce5c540766758243a80d2233
maksimspiridonov <maksim.spiridon0v@yandex.ru>
2024-11-26 21:16:27 +0500

    4

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 export default defineConfig({
   plugins: [react(), basicSsl()],
   build: {
-    outDir: './root'
+    outDir: './docs'
   },
   base: '/casino-bot/'
 });
